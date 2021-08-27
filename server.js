05495f3a59fb21b9e4eea29bfd43811ee9c4c138
Marcelo Lopez Gallo <melopez@teco.com.ar>
2021-08-27 11:56:01 -0300

    se agrega env.process.PORT

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require('express')
 const contenedor = new Contenedor('./productos.txt')
 const app = express()
 
-const PORT = 8080
+const PORT = env.process.PORT || 8080
 
 const server = app.listen(PORT, () => {
     console.log(`Servidor http escuchando en el puerto ${server.address().port}`)
